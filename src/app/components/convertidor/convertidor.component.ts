bd8bf852b114a0708b16480cd5047b014daa24bd
zassory <nicolas.programador@gmail.com>
2023-03-09 23:39:15 -0300

    funcion que calcula ok

--- src/app/components/convertidor/convertidor.component.ts
+++ src/app/components/convertidor/convertidor.component.ts
@@ -11,11 +11,49 @@ export class ConvertidorComponent implements OnInit {
   quiero='EUR';
   total=0;
   
-  monedas: string[] = ['USD', 'EUR', 'LIBRA','CLP'];
+  monedas: string[] = ['USD', 'EUR', 'LIBRA'];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  convertir():void{
+    switch(this.tengo) {
+      case 'USD':
+        if(this.quiero === 'USD'){
+          this.total = this.cantidad;
+        }
+        if(this.quiero === 'EUR'){
+          this.total = this.cantidad * 0.84;
+        }
+        if(this.quiero === 'LIBRA'){
+          this.total = this.cantidad * 0.75;
+        }
+        break;
+      case 'EUR':
+        if(this.quiero === 'EUR'){
+          this.total = this.cantidad;
+        }
+        if(this.quiero === 'USD'){
+          this.total = this.cantidad * 1.20;
+        }
+        if(this.quiero === 'LIBRA'){
+          this.total = this.cantidad * 0.90;
+        }
+        break;
+      case 'LIBRA':
+        if(this.quiero === 'LIBRA'){
+          this.total = this.cantidad;
+        }
+        if(this.quiero === 'USD'){
+          this.total = this.cantidad * 1.33;
+        }
+        if(this.quiero === 'EUR'){
+          this.total = this.cantidad * 1.11;
+        }
+        break;      
+    }
   }
 
 }
